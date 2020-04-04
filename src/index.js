d8bc75dad60a65ab0979e717a46638b461b45ce3
Magda Cembor <magda.cembor@gmail.com>
2020-04-04 16:18:05 +0100

    reusable components and children; using props within components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,57 @@
-// Import react and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-// Create a react component
+
 const App = () => {
-    return <div>Hi There!</div>;
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail 
+          author='Sam' 
+          timeAgo='Today at 4pm' 
+          title='Awesome'
+          avatar={faker.image.avatar()}
+          />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author='Ben' 
+          timeAgo='Today at 6pm' 
+          title='Hey hey'
+          avatar={faker.image.avatar()}
+          />
+        </ApprovalCard>
+        <ApprovalCard>
+        <CommentDetail 
+          author='Alex' 
+          timeAgo='Yesterday at 10pm' 
+          title='Bacon!'
+          avatar={faker.image.avatar()}
+          />
+      </ApprovalCard>
+    </div>
+  )
 }
-
-// Take the react component and show it on a screen
 
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
 );
+
+
+
+// const App = () => {
+//   const buttonText = { text: 'Click Me!' };
+//   const style = {backgroundColor: 'blue', color: 'white'};
+
+//   return (
+//     <div>
+//       <label className="name" htmlFor="name">Enter name:</label>
+//       <input id="name" type="text" />
+//       <button style={style}>{buttonText.text}</button>
+//     </div>
+//   )
+// }
